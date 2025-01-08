ededdbb31deb1eb8c6b52eb886e13b26f743e9ac
vercyngetoryks <michal.krakowinski.gmail.com>
2025-01-08 11:47:42 +0100

    create gabebord object and createUser factory

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+"use strict";
 
+const gameboard = {
+  board: ["", "", "", "", "", "", "", "", ""],
+};
+
+function createUser(name, marker, active) {
+  return { name, marker, active };
+}
+
+const playerOne = createUser("Bob", "O", true);
+const playerTwo = createUser("John", "X", false);
+
+const players = [playerOne, playerTwo];
+
+console.log(playerOne, playerTwo);
